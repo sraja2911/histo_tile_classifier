ce23a5fe5b701ccfeef3d97ef949ee696005caa0
Raj Manian <sraja2911@gmail.com>
2018-09-06 15:14:33 -0400

    Updated tiles4cnn
    Updated tilesPulling4CNN

--- tile_segmentation.sh
+++ tile_segmentation.sh
@@ -1,8 +1,8 @@
-docker run \
+nvidia-docker run \
      -it --rm -p3333:8888 \
      -v "${PWD}:/app:rw" \
      -v "/home/raj/github/histo_tile_classifier:/data/code:rw" \
      -v "/home/raj/github/histo_tile_classifier/results:/data/output/results:rw" \
-     -v "/media/raj/Raj1_5/histo_tile_50k_data/train:/data/train:rw" \
-     -v "/media/raj/Raj1_5/histo_tile_50k_data/test:/data/test:rw" \
+     -v "/media/raj/Raj1_5/tiles_data/train:/data/train:rw" \
+     -v "/media/raj/Raj1_5/tiles_data/test:/data/test:rw" \
      fgiuste/histomicstk
